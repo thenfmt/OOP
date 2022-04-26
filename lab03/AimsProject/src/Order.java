63b8243816e7d40760e329e83f612ab4df6ee869
thenfmt <thenf.mt@gmail.com>
2022-04-27 00:34:06 +0700

    update

--- lab03/AimsProject/src/Order.java
+++ lab03/AimsProject/src/Order.java
@@ -2,19 +2,26 @@
 public class Order {
 	public static final int Max_Order = 10;
 	private DVD itemsOrdered[] = new DVD[Max_Order];
-	int index = 0;
+	int nbOrdered = 0;
 	
 	public void addDVD(DVD dvd) {
-		itemsOrdered[index++] = dvd;
+		itemsOrdered[nbOrdered++] = dvd;
 	}
 	
 	public void removeDVD(DVD dvd) {
-		index--;
+		for(int i = 0; i < nbOrdered; i++) {
+			if(itemsOrdered[i]== dvd) {
+				for(int j = i; j < nbOrdered-1; j++) {
+					itemsOrdered[j] = itemsOrdered[j+1];
+				}
+				nbOrdered--;
+			}
+		}
 	}
 	
 	public float totalCost() {
 		float cost = 0;
-		for(int i = 0; i < index; i++) {
+		for(int i = 0; i < nbOrdered; i++) {
 			cost += itemsOrdered[i].getCost();
 		}
 		return cost;
